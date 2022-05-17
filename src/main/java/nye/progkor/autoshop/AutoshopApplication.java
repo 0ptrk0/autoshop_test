e77c8e993014f0b7d02a55a18165ca5ebd911511
PTRK <kovacspatrik02@gmail.com>
2022-05-17 16:59:53 +0200

    test

--- src/main/java/nye/progkor/autoshop/AutoshopApplication.java
+++ src/main/java/nye/progkor/autoshop/AutoshopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutoshopApplication {
-
+//test
   public static void main(String[] args) {
     SpringApplication.run(AutoshopApplication.class, args);
   }
